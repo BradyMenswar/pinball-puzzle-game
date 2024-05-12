46174665a49fe157648dc96b520e969c450b906e
Brady Menswar <bradymenswar@gmail.com>
2024-05-12 11:55:14 -0400

    feat: made max speed for ball; limited camera movement to only follow vertically

--- Ball.gd
+++ Ball.gd
@@ -1,7 +1,9 @@
 extends RigidBody2D
 
 var reset_state = false
-var moveVector: Vector2
+var reset_location
+@export var max_speed = 100
+
 
 func _input(event):
 	if event.is_action_pressed("reset_ball"):
@@ -9,11 +11,16 @@ func _input(event):
 
 func _integrate_forces(state):
 	if reset_state:
-		state.transform = Transform2D(0.0, moveVector)
+		state.transform = Transform2D(0.0, reset_location)
 		reset_state = false
 		angular_velocity = 0
 		linear_velocity = Vector2.ZERO
+		
+func _physics_process(delta):
+	print("Velocity: ", linear_velocity)
+	if(linear_velocity.length() > max_speed):
+		linear_velocity = linear_velocity.normalized() * max_speed
 
-func move_body(targetPos: Vector2):
-	moveVector = targetPos;
-	reset_state = true;
+func move_body(target_location: Vector2):
+	reset_location = target_location
+	reset_state = true
