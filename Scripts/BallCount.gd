5c4e3361e802eefef5e48d9a7bf1efffe9d11aab
nelsonwjohn <157164763+nelsonwjohn@users.noreply.github.com>
2024-05-14 13:43:51 -0400

    Added Button with Active/Dormant State

--- Scripts/BallCount.gd
+++ Scripts/BallCount.gd
@@ -1,6 +1,5 @@
 extends Label
 
-#Updates Ball Count
-func _on_killbox_change_ballcount(ballcount):
-	text = "Balls Lost: " + str(ballcount)
+func _on_head_1_change_status():
+	text = "Head1: Active"
 	pass # Replace with function body.
